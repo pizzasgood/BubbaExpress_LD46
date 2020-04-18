f76310cbc5ce000b310c6e097f0b591762cad8b2
pizzasgood <pizzasgood@gmail.com>
2020-04-18 12:53:50 -0400

    Make acid translucent and do damage

--- terrain/Acid.gd
+++ terrain/Acid.gd
@@ -1,4 +1,17 @@
 extends Area2D
 
+var initial_damage = 10
+var ongoing_damage = 50
+
+onready var player = get_tree().get_nodes_in_group("player")[0]
+
 func _ready():
-	pass
+	connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(object):
+	if object.is_in_group("player"):
+		object.damage(initial_damage)
+
+func _process(delta):
+	if overlaps_body(player):
+		player.damage(ongoing_damage * delta)
