737657328b14686bc1f9abee6779877e24203cb4
pizzasgood <pizzasgood@gmail.com>
2020-04-19 10:44:16 -0400

    Highlight drop points when active

--- Mountpoint.gd
+++ Mountpoint.gd
@@ -9,10 +9,12 @@ func _ready():
 func _on_body_entered(body):
 	if body.has_method("set_drop_zone"):
 		body.set_drop_zone(true, self)
+		$Sprite.modulate = Color(1.25, 1.25, 1.25, 1)
 
 func _on_body_exited(body):
 	if body.has_method("set_drop_zone"):
 		body.set_drop_zone(false, self)
+		$Sprite.modulate = Color(1, 1, 1, 1)
 
 func accepts_item(item) -> bool:
 	return "upgrade" in item and mounted_item == null
